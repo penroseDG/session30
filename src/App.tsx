e73d60c8a1645006ec2594b21a32cf38eb00463c
Đào Đức <dudangdaide@gmail.com>
2024-05-16 07:54:47 +0700

    baitapss30

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Function from './components/FunctionComonent.tsx/function'
+import Ex1 from './components/FunctionComonent.tsx/Ex1'
+import Ex2 from './components/FunctionComonent.tsx/Ex2'
+import Ex3 from './components/FunctionComonent.tsx/Ex3'
+import Ex4 from './components/FunctionComonent.tsx/Ex4'
+import Ex5 from './components/FunctionComonent.tsx/Ex5'
+import Ex6 from './components/FunctionComonent.tsx/Ex6'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      aaaa
+      <Function></Function>
+      <Ex1></Ex1>
+      <Ex2></Ex2>
+      <Ex3></Ex3>
+      <Ex4></Ex4>
+      <Ex5></Ex5>
+      <Ex6></Ex6>
+    </div>
   )
 }
-
-export default App
